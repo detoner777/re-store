1ed9ce55912f967d77ba494f51744455c3936a9d
Alex Lisenko <detoner777@gmail.com>
2019-06-18 11:36:21 +0300

    Basic functionaliti complit

--- src/components/shopping-cart-table/shopping-cart-table.js
+++ src/components/shopping-cart-table/shopping-cart-table.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
+import {
+  bookAddedToCart,
+  bookRemoveFromCart,
+  allBooksRemovedFromCart } from '../../actions';
+
 import './shopping-cart-table.css';
 
 const ShoppingCartTable = ({ items, total, onIncrease, onDecrease, onDelete }) => {
   const renderRow = (item, idx) => {
-    const { id, name, count, total } = item;
+    const { id, title, count, total } = item;
     return (
       <tr key={id}>
         <td>{idx + 1}</td>
-        <td>{name}</td>
+        <td>{title}</td>
         <td>{count}</td>
         <td>${total}</td>
         <td>
@@ -65,20 +71,10 @@ const mapStateToProps = ({ cartItems, orderTotal }) => {
   };
 };
 
-const mapDispatchToProps = () => {
-  return {
-    onIncrease: (id) => {
-      console.log(`Increase ${id}`);
-    },
-
-    onDecrease: (id) => {
-      console.log(`Decrease ${id}`);
-    },
-
-    onDelete: (id) => {
-      console.log(`Delete ${id}`);
-    }
-  }
-};
+const mapDispatchToProps = {
+    onIncrease: bookAddedToCart,
+    onDecrease: bookRemoveFromCart,
+    onDelete: allBooksRemovedFromCart
+    };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ShoppingCartTable);
